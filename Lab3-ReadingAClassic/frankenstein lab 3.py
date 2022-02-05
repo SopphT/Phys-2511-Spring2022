57e53240b452cf347aa530676c3e9ec1762201da
Sophia Tenaglia <97555891+SopphT@users.noreply.github.com>
2022-02-04 21:53:51 -0500

    Update frankenstein lab 3.py

--- Lab3-ReadingAClassic/frankenstein lab 3.py
+++ Lab3-ReadingAClassic/frankenstein lab 3.py
@@ -5,12 +5,15 @@ Created on Wed Feb  2 22:47:11 2022
 @author: sophi
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 full_text = open("frankenstein.txt", "r", encoding="utf8")
 
 read_file = False
 
-character_names = ["Victor", "creature","Agatha","Caroline","Lacey","Elizabeth"
-                   ,"Ernest","Felix","Henry","Justine","William"]
+character_names = ["Victor","Agatha","Caroline","Lacey","Elizabeth"
+                   ,"Ernest","Felix","Henry","Justine","William","TheCreature"]
 
 book_text = []
 for line in full_text:
@@ -19,61 +22,54 @@ for line in full_text:
     if "*** END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN ***" in line:
         read_file = False
     if read_file == True:
-        book_text.append(line)
+        book_text.append(line.lower())
 book_text[0] = " "
+full_text.close()
 
-#4 letters
-#24 chapters
-letter_number = 1
-for letter_number in range(1,5):
-    chapter_text = []
-    for line in book_text:
-        if "Letter " + str(letter_number) in line:
-            read_file = True
-        if "Letter " + str(letter_number + 1) in line or "Chapter 1" in line:
-            read_file = False
-        if read_file == True:
-            string_of_words = line.split()
-            for word in string_of_words:
-                word = word.strip(".,:;\"?!\'*)( ")
-                chapter_text.append(word)
-    print("\nLetter " + str(letter_number) + "\n")            
-    for name in character_names:
-        print(name, chapter_text.count(name))
-        
+for i in range(0, len(book_text)):
+    book_text[i] = book_text[i].replace("the creature", "thecreature")
+
+labels = []
+name_counter = []
+
 chapter_number = 1
 for chapter_number in range(1,25):
+    labels.append(str(chapter_number))
     chapter_text = []
     for line in book_text:
-        if "Chapter " + str(chapter_number) in line:
+        if "chapter " + str(chapter_number) in line:
             read_file = True
-        if "Chapter " + str(chapter_number + 1) in line:
+        if "chapter " + str(chapter_number + 1) in line:
             read_file = False
         if read_file == True:
             string_of_words = line.split()
             for word in string_of_words:
                 word = word.strip(".,:;\"?!\'*)( ")
                 chapter_text.append(word)
-    print("\nChapter " + str(chapter_number) + "\n")            
+    print("\nChapter " + str(chapter_number) + "\n")
+    this_chapter_counts = []            
     for name in character_names:
-        print(name, chapter_text.count(name))
-        
-    
-   
+        print(name, chapter_text.count(name.lower()))
+        this_chapter_counts.append(chapter_text.count(name.lower()))
+    name_counter.append(this_chapter_counts)
 
-    
+# swap rows and columns
+name_counter = np.array(name_counter).T
 
+# time to plot
+fig, ax = plt.subplots()
+i = 0
+for name_counts in name_counter:
+    ax.plot(range(1,25),name_counts, label = character_names[i], linewidth = 3)
+    i = i + 1
 
-        
-        
-"""
-string_of_words = line.split()
-        
-        
-      
-    for word in string_of_words:
-        word = word.strip(".,:;\"?!\'*)( ")
-        list_of_words.append(word)
-        """
-# *** END OF THE PROJECT GUTENBERG EBOOK FRANKENSTEIN ***
+ax.set_ylabel('Times Name Mentioned Per Chapter')
+ax.set_xlabel('Chapter Number')
+ax.set_title('Names in Frankenstein')
+ax.set_xticks(range(0,25))
+ax.legend()
 
+plt.show()
+
+#I hope it's okay that I used https://matplotlib.org/stable/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+#I know this website was mentioned as a resource in class, but I was unsure if that also made it a resource for the lab
